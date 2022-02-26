46c0a0001f16a005bc564bdf4da22ca96e60fe83
Jagannath <jneupane07@gmail.com>
2022-02-26 17:15:34 +1100

    first attempt

---
+++ faultyCalculator.py
@@ -0,0 +1,35 @@
+firstNumber = int(input("Enter a first number"))
+secondNumber = int(input("enter a second number"))
+addition = firstNumber+secondNumber
+subtraction = firstNumber-secondNumber
+multiplication = firstNumber*secondNumber
+division = firstNumber/secondNumber
+if firstNumber==45 and secondNumber==3:
+    faultyanswer =555
+    print("Multiplication of",firstNumber," and",secondNumber,"is",faultyanswer)
+    print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
+    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
+    print("Division of",firstNumber," and",secondNumber,"is",division)
+elif firstNumber==56 and secondNumber==9:
+    faultyanswer= 77
+    print("Addition of",firstNumber," and",secondNumber,"is",faultyanswer)
+    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
+    print("Division of",firstNumber," and",secondNumber,"is",division)
+    print("Multiplication of",firstNumber," and",secondNumber,"is",multiplication)
+elif firstNumber==56 and secondNumber==6:
+    faultyanswer=4
+    print("Division of",firstNumber," and",secondNumber,"is",faultyanswer)
+    print("Multiplication of",firstNumber," and",secondNumber,"is",multiplication)
+    print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
+    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
+else:
+    print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
+    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
+    print("Multiplication of",firstNumber," and",secondNumber,"is",multiplication)
+    print("Division of",firstNumber," and",secondNumber,"is",division)
+
+
+
+
+
+
