4059bcadfccee1f4105cbe081997ff5cda11e03c
Jagannath Neupane <jneupane07@gmail.com>
2022-03-02 06:59:52 +1100

    Update faultyCalculator.py
    Few codes edited

--- faultyCalculator.py
+++ faultyCalculator.py
@@ -4,29 +4,37 @@ addition = firstNumber+secondNumber
 subtraction = firstNumber-secondNumber
 multiplication = firstNumber*secondNumber
 division = firstNumber/secondNumber
-if firstNumber==45 and secondNumber==3:
-    faultyanswer =555
-    print("Multiplication of",firstNumber," and",secondNumber,"is",faultyanswer)
-    print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
-    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
-    print("Division of",firstNumber," and",secondNumber,"is",division)
-elif firstNumber==56 and secondNumber==9:
-    faultyanswer= 77
-    print("Addition of",firstNumber," and",secondNumber,"is",faultyanswer)
-    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
-    print("Division of",firstNumber," and",secondNumber,"is",division)
-    print("Multiplication of",firstNumber," and",secondNumber,"is",multiplication)
-elif firstNumber==56 and secondNumber==6:
-    faultyanswer=4
-    print("Division of",firstNumber," and",secondNumber,"is",faultyanswer)
-    print("Multiplication of",firstNumber," and",secondNumber,"is",multiplication)
-    print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
-    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
+choice = input("Enter \n 1 for addition \n 2  for subtraction \n 3  for division \n 4 for multiplication\t")
+
+if choice=='1':
+    if firstNumber==56 and secondNumber==9:
+        faultyanswer =77
+        print("Addition of",firstNumber, "and",secondNumber,"is",faultyanswer)
+    else:
+        print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
+
+
+elif choice=='2':
+    print("Subtraction of",firstNumber,"and",secondNumber,"is",subtraction)
+
+
+elif choice=='3':
+    if firstNumber==56 and secondNumber==6:
+        faultyanswer = 4
+    else:
+        print("Division of",firstNumber,"with",secondNumber,"is",division)
+elif choice=='4':
+    if firstNumber==45 and secondNumber==3:
+        faultyanswer=555
+        print("Multiplication of",firstNumber,"and",secondNumber,"is",faultyanswer)
+    else:
+        print("Multiplication of",firstNumber,"and",secondNumber,"is",multiplication)
+
 else:
-    print("Addition of ",firstNumber,"and", secondNumber,"is",addition)
-    print("Subtraction of",firstNumber," and",secondNumber,"is",subtraction)
-    print("Multiplication of",firstNumber," and",secondNumber,"is",multiplication)
-    print("Division of",firstNumber," and",secondNumber,"is",division)
+    print("Wrong selection, Try again")
+
+
+
 
 
 
